f12eb4ebd6eaa1d576e434d8480dd43c62c1d0d6
Frank <this.comp2025@gmail.com>
2026-03-30 00:23:57 +0300

    update .env,.gitignore,Docker-compose.yml,backend/Dockerfile,poetry.lock,pyproject.toml and Add backend/entrypoint.sh

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/sh
+
+# Apply database migrations
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Gunicorn server
+exec gunicorn quiz_time.wsgi:application --bind 0.0.0.0:8000 --workers 4
